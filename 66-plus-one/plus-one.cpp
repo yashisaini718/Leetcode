566ed1f3a3abe1abe467bd75fa6777bff3e66e3b
Yashi Saini <yashisaini718@gmail.com>
2024-12-14 11:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (15.71%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       int n=digits.size();
+       int i=n-1;
+       while(i>=0){
+        if(digits[i]==9){
+            digits[i]= 0;
+        }
+        else{
+            digits[i]+=1;
+            return digits;
+        }
+        i--;
+       }
+       digits.insert(digits.begin(),1);
+       return digits;
+    }
+};
