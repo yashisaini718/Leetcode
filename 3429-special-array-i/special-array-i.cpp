e6f366b71b44018a5ecf19558714c0d59006a074
Yashi Saini <yashisaini718@gmail.com>
2025-02-01 20:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if((nums[i-1]%2==0) && (nums[i]%2!=0)){
+                continue;
+            }
+            else if(nums[i-1]%2!=0 && nums[i]%2==0){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
