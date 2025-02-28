60c2487130f63bb4449b7277f9fb3bc10b4726b6
Yashi Saini <yashisaini718@gmail.com>
2025-02-28 08:25:45 +0530

    Time: 1 ms (60.67%) | Memory: 8.5 MB (92.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -7,7 +7,7 @@ public:
         if(x==0){
             return true;
         }
-        /*long long y=0 ;
+        long long y=0 ;
         long long n=x;
         while(x!=0){
             int d=x%10;
@@ -20,12 +20,18 @@ public:
         else{
             return false;
         }
-        */
+        /*
+        TC of loop approach: O(log10(x)) everytime the number is divided by x
+        SC: O(1)
         string num= to_string(x);
         reverse(num.begin(),num.end());
         long long y=stoll(num);
         long long z=x;
         if(y==z) return true;
         else return false;
+        TC of string based: O(log10(x))
+        SC: O(n) n;length of string 
+        hence loop approach is more optimized
+        */
     }
 };
