16c6a9db3c00ec5202f8dc8868afbacfd5328bcd
Yashi Saini <yashisaini718@gmail.com>
2026-06-17 13:32:59 +0530

    Time: 16 ms (95.4%), Space: 24 MB (67.82%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n=s.length();
+        // instead of building the whole result string build the length of the result
+        long long L= 0;
+        for(char &ch : s){
+            if(ch == '%'){
+                continue;
+            } else if (ch == '*'){
+                if (L >= 1) L--;
+            } else if (ch == '#'){
+                L*=2;
+            } else {
+                L++;
+            }
+        }
+
+        if (k >= L) return '.';
+        
+        // backtrack each of the operation to map the required character in the result to a character in s
+        for(int i= n-1; i>=0; i--) {
+            if (s[i] == '#'){
+                L= L/2;
+                k= (k >= L)? k-L: k;
+            }
+            else if (s[i] == '%'){
+                // no change in L
+                k= L-k-1;
+            }
+            else if (s[i] == '*'){
+                L++;
+                // no change in k
+            }
+            else{//letter
+                L--;
+                // no change in k
+            }
+            // after doing all these check if L==k 
+            if(L == k) return s[i];
+        }
+        return '.';
+    }
+
+    // char processStr(string s, long long k) {
+    //     int n = s.length();
+
+    //     long long L = 0;
+
+    //     for(char &ch : s) {
+    //         if(ch == '*') {
+    //             if(L > 0)
+    //                 L--;
+    //         } else if(ch == '#') {
+    //             L *= 2;
+    //         } else if(ch == '%') {
+    //             //reverse - no change in L
+    //             continue;
+    //         } else { //'a' to 'z'
+    //             L++;
+    //         }
+    //     }
+
+    //     if(k >= L) {
+    //         return '.';
+    //     }
+
+    //     for(int i = n-1; i >= 0; i--) {
+    //         if(s[i] == '*') {
+    //             L++; //no change in k
+    //         } else if(s[i] == '%') {
+    //             //no change in L
+    //             k = L - k - 1;
+    //         } else if(s[i] == '#') {
+    //             L = L/2;
+    //             k = (k >= L) ? k - L : k;
+    //         } else { //'a' to 'z'
+    //             L--;
+    //         }
+
+    //         if(k == L) {
+    //             return s[i];
+    //         }
+    //     }
+
+    //     return '.';
+
+    // }
+};
