5a0ade4fbdc110d4768b2de293f0d8f88221005a
Yashi Saini <yashisaini718@gmail.com>
2026-06-18 19:29:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (46.25%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        //each min, min hand move 6 degree while the hour hand move (30/60)=0.5 degree
-        // each hour the hour hand moves 30 degree
+        // minhand= 360 degree / 60 = 6 degree/min
+        // each min, min hand move 6 degree while the hour hand move 
+        // 1 hour equals 30 degree angle and 1 hour equal 60 min 
+        // so hour hand move (30/60)=0.5 degree/min
+        // each hour the hour hand moves 30 degree/hour
         double hourangle= hour*30 + minutes*0.5;
         double minangle= minutes*6;
         double angle= abs(hourangle-minangle);
