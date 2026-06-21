015e03aa0c5170119282fac9ce18ad5f5f26a43c
Yashi Saini <yashisaini718@gmail.com>
2026-06-21 18:25:45 +0530

    Time: 207 ms (28.57%), Space: 360.7 MB (14.29%) - LeetHub

--- 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
+++ 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
@@ -1,10 +1,19 @@
 class Solution {
+    // making a Tree map to store tree nodes with its child
+    // made a finishTime array to stre the time of each node
+    // for each leafnode the finishtime is equal to baseTime
+    // for each nonleaf node the finishtime can be calculated using a for loop and keeping track of min and max across the children to get the required finishTime of node 
+    // remember to use long long for all tasks
+
     typedef long long ll;
+
     ll dfs(ll node, unordered_map<ll,vector<ll>>&Tree, vector<int>&baseTime, vector<ll>&finishTime){
+        //basecase for leaf node
         if(Tree.find(node) == Tree.end()){
             finishTime[node]=baseTime[node];
             return finishTime[node];
         }
+
         ll minTime=LLONG_MAX;
         ll maxTime=LLONG_MIN;
         for(ll child : Tree[node]){
@@ -12,6 +21,7 @@ class Solution {
             minTime=min(minTime,childfinish);
             maxTime=max(maxTime,childfinish);
         }
+
         finishTime[node]= 2 * maxTime - minTime + baseTime[node];
         return finishTime[node];
     }
@@ -19,9 +29,12 @@ public:
     long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
         unordered_map<ll,vector<ll>>Tree;
         vector<ll>finishTime(n,0);
+
+        // building the tree
         for(auto &node : edges){
             Tree[node[0]].push_back(node[1]);
         }
+        
         return dfs (0, Tree, baseTime, finishTime);
     }
 };
