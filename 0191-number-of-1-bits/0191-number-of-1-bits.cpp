fcb2203ec5328f0db9e5f77a312e472a04f28a1a
Yashi Saini <yashisaini718@gmail.com>
2026-07-16 17:40:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.73%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __popcount(n);
+        int cnt=0;
+        while(n>0){
+            if(n&1 == 1) cnt++;
+            n=n>>1;
+        }
+        return cnt;
     }
 };
