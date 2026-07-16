5d9262896f26fbe5930a4eb004cc305f2f7454c5
Yashi Saini <yashisaini718@gmail.com>
2026-07-16 17:49:10 +0530

    Time: 3 ms (2.57%), Space: 8.3 MB (48.73%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,5 +2,8 @@ class Solution {
 public:
     int hammingWeight(int n) {
         return bitset<32>(n).count();
+        // the count function of bitset works as same as __builtin_popcount
+        // this creates a bitset of 32 size for the number n 
+        // and count gives the count of set bits in bitset of n
     }
 };
