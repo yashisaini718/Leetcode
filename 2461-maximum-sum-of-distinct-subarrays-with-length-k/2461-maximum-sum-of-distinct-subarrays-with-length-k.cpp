621caf43590f2e9e4bf5ce99a39d6722245a2ef1
Yashi Saini <yashisaini718@gmail.com>
2026-07-16 19:02:38 +0530

    Time: 210 ms (8.57%), Space: 104.2 MB (8.8%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,31 +1,39 @@
 class Solution {
+    // no need for this helper function just a small tweak ... use freq.size to count no. of distinct elements in the subarray instead of running a full iteration
+
     bool distinct(map<int,int>&freq){
         for(auto it:freq){
             if(it.second != 1) return false;
         }
         return true;
     }
+
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
+
         map<int,int>freq;
         long long sum=0;
         long long maxi=0;
+
         // first window
         for(int i=0;i<k;i++){
             sum+=nums[i];
             freq[nums[i]]++;
         }
         if(freq.size() == k )maxi=max(maxi,sum);
-        // i is moved via end of the window hence it shoudl go all the way to the end of the array
+
+        // i is moved via end of the window hence it should go all the way to the end of the array
         for(int i=k;i<n;i++){
             freq[nums[i]]++;
             sum-=nums[i-k];
             sum+=nums[i];
             if(freq[nums[i-k]]==1)freq.erase(nums[i-k]);
             else freq[nums[i-k]]--;
+
             if(freq.size() == k) maxi=max(maxi,sum);
         }
+        
         return maxi;
     }
 };
