10a8fbb94a1e6524ac1a7a900da713c487cd7cc9
Yashi Saini <yashisaini718@gmail.com>
2026-07-27 14:45:23 +0530

    Time: 4 ms (51.5%), Space: 15.2 MB (9.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // making a hash map to keep the counter of a val with it's index 
+        // and as move forward check if target-nums[i] present in map or not 
+        int n=nums.size();
+        map<int,int>hash;
+        for(int i=0;i<n;i++){
+            int req=target-nums[i];
+            if (hash.count(req)){
+                return {hash[req],i};
+            }
+            hash[nums[i]]=i;
+        }
+        return {};
+    }
+};
