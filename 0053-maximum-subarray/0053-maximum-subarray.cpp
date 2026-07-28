4f212ce5e806cf960d5f13645b134c784f79d7e5
Yashi Saini <yashisaini718@gmail.com>
2026-07-28 10:40:09 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.86%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,7 +3,6 @@ class Solution {
     // In standard Kadane we use to set to zero if negative occurs but in this since the array could have all negative numbers and we need to return the largest negative in that case we should get the sum via comparison with nums[i] instead of zero
 public:
     int maxSubArray(vector<int>& nums) {
-        // if sum becomes negative use sum=0
         int n = nums.size();
         int sum = 0;
         int maxi = INT_MIN;
