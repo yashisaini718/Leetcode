44c3108ebaa1c35e8830012caf5ed91f39c64872
Yashi Saini <yashisaini718@gmail.com>
2026-07-28 09:39:28 +0530

    Time: 1 ms (34.9%), Space: 28.4 MB (12.31%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement( vector <int> & nums ) {
+        int n = nums.size() ;
+        unordered_map <int, int> freq ;
+        for (int i = 0 ; i < n ; i++ ){
+            freq [ nums[i] ] ++ ;
+        }
+        for ( auto &it  : freq ){
+            if ( it.second > n/2 ) return it.first ;
+        }
+        return -1 ;
+    }
+};
