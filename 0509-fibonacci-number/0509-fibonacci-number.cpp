9a3c816ee3fad16be866dcbcb8f82f11326e2713
Yashi Saini <yashisaini718@gmail.com>
2026-08-07 15:35:53 +0530

    Time: 2 ms (77.79%), Space: 7.8 MB (39.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
+    int t[32];
+    int solve(int n){
+        if (n == 0) return t[n] = 0;
+        if (n == 1) return t[n] = 1;
+        if (t[n] != -1) return t[n];
+        return t[n]= solve(n-1) + solve(n-2);
+    }
 public:
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+        memset(t, -1, sizeof(t));
+        return solve(n);
     }
 };
