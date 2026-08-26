e7bb50f75d456d88895bd06a3dff6458fea12c42
Yashi Saini <yashisaini718@gmail.com>
2026-08-26 17:46:59 +0530

    Time: 22 ms (15.64%), Space: 14.7 MB (55.14%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.length();
-        int hash[26] = {0};
-        for(int i=0; i<n; i++){
-            hash[s[i]-'a']++;
-        }
-        for(int i=0;i<n;i++){
-            if(hash[s[i]-'a'] == 1) return i;
-        }
-        return -1;
-        // unordered_map<char,pair<int,int>>mpp;
-        // for(int i=n-1; i>=0; i--){
-        //     mpp[s[i]].first++;
-        //     mpp[s[i]].second = i;
+        // int hash[26] = {0};
+        // for(int i=0; i<n; i++){
+        //     hash[s[i]-'a']++;
         // }
-        // for(auto &itr : mpp){
-        //     if (itr.second.first == 1){
-        //         return itr.second.second;
-        //     }
+        // for(int i=0;i<n;i++){
+        //     if(hash[s[i]-'a'] == 1) return i;
         // }
         // return -1;
+        unordered_map<char,pair<int,int>>mpp;
+        for(int i=n-1; i>=0; i--){
+            mpp[s[i]].first++;
+            mpp[s[i]].second = i;
+        }
+        for(int i=0; i<n; i++){
+            if (mpp[s[i]].first == 1){
+                return i;
+            }
+        }
+        return -1;
     }
 };
