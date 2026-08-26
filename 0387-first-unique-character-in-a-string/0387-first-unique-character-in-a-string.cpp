6b09c5f7d1571d05e0dd3eb34050f93a55bf51eb
Yashi Saini <yashisaini718@gmail.com>
2026-08-26 17:43:51 +0530

    Time: 4 ms (72.69%), Space: 14.6 MB (87.82%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.length();
-        int hash[26];
+        int hash[26] = {0};
         for(int i=0; i<n; i++){
             hash[s[i]-'a']++;
         }
