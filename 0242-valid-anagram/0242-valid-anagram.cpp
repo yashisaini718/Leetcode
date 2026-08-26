65bfd28ef03d4d66c26d94e44ce51bac14125bbc
Yashi Saini <yashisaini718@gmail.com>
2026-08-26 16:52:42 +0530

    Time: 3 ms (45.05%), Space: 9.7 MB (75.72%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        // approach 1: sort both and compare
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        int i = 0, j = 0;
+        while( i < s.length() && j < t.length()){
+            if (s[i] != t[j]) return false;
+            i++;
+            j++;
+        }
+        return true;
+        // if (s == t) return true;
+        // return false;
+    }
+};
