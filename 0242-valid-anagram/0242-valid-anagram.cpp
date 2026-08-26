a4bed3ef6974c8a823bc592e1ff1e305695d8863
Yashi Saini <yashisaini718@gmail.com>
2026-08-26 16:53:30 +0530

    Time: 7 ms (18.1%), Space: 9.6 MB (75.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,13 +6,13 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         int i = 0, j = 0;
-        while( i < s.length() && j < t.length()){
-            if (s[i] != t[j]) return false;
-            i++;
-            j++;
-        }
-        return true;
-        // if (s == t) return true;
-        // return false;
+        // while( i < s.length() && j < t.length()){
+        //     if (s[i] != t[j]) return false;
+        //     i++;
+        //     j++;
+        // }
+        // return true;
+        if (s == t) return true;
+        return false;
     }
 };
