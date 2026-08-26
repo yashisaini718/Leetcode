09f525dda434899171dda9a51221cd6f9b0dae69
Yashi Saini <yashisaini718@gmail.com>
2026-08-26 17:00:51 +0530

    Time: 10 ms (6.82%), Space: 9.8 MB (49.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
-        // approach 1: sort both and compare
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        int i = 0, j = 0;
+        // approach 1: sort both and compare : TC O(nlogn)
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // int i = 0, j = 0;
         // while( i < s.length() && j < t.length()){
         //     if (s[i] != t[j]) return false;
         //     i++;
         //     j++;
         // }
         // return true;
-        if (s == t) return true;
-        return false;
+        // if (s == t) return true;
+        // return false;
+
+        map<char,int>m1;
+        map<char,int>m2;
+        for(char ch : s) m1[ch]++;
+        for(char ch : t) m2[ch]++;
+        for(char ch : s){
+            if (m1[ch] != m2[ch]) return false;
+        }
+        return true;
     }
 };
