fd33704b78c84244eddd70eb73a71dfb6926b939
Yashi Saini <yashisaini718@gmail.com>
2026-08-29 21:33:36 +0530

    Time: 8 ms (88.22%), Space: 16.2 MB (52.78%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     string sortVowels(string s) {
         int n = s.length();
-        vector<char>vowels;
+        //vector<char>vowels;
+        vector<int>frq(128,0);
         string v = "AEIOUaeiou";
         for(int i = 0 ; i < n ; i++ ){
             if(v.find(s[i]) != string::npos){
-                vowels.push_back(s[i]);
+                //vowels.push_back(s[i]);
+                frq[s[i]]++;
             }
         }
-        sort(vowels.begin(),vowels.end());
+        //sort(vowels.begin(),vowels.end());
         int j = 0;
-        for(int i = 0 ; i < n ; i++ ){
+        for(int i = 0 ; i <n ; i++ ){
             if(v.find(s[i]) != string::npos){
-                s[i] = vowels[j++];
+                while(frq[v[j]] == 0) j++;
+                //s[i] = vowels[j++];
+                s[i] = v[j];
+                frq[v[j]]--;
             }
         }
         return s;
