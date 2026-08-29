dbcda9bb07e1bf1aa918fc8ea84bb5775af3f516
Yashi Saini <yashisaini718@gmail.com>
2026-08-29 21:34:13 +0530

    Time: 7 ms (91.94%), Space: 16.1 MB (54.26%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     string sortVowels(string s) {
         int n = s.length();
+        
         //vector<char>vowels;
+
         vector<int>frq(128,0);
+
         string v = "AEIOUaeiou";
+
         for(int i = 0 ; i < n ; i++ ){
             if(v.find(s[i]) != string::npos){
                 //vowels.push_back(s[i]);
                 frq[s[i]]++;
             }
         }
+
         //sort(vowels.begin(),vowels.end());
+
         int j = 0;
+
         for(int i = 0 ; i <n ; i++ ){
             if(v.find(s[i]) != string::npos){
                 while(frq[v[j]] == 0) j++;
@@ -21,6 +28,7 @@ public:
                 frq[v[j]]--;
             }
         }
+
         return s;
     }
 };
