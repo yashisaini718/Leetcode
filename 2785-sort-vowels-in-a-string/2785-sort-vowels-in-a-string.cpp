d0bf604185fdb949b830b1a774f48a331aa8749d
Yashi Saini <yashisaini718@gmail.com>
2026-08-29 21:28:08 +0530

    Time: 19 ms (37.11%), Space: 16 MB (54.26%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -3,15 +3,16 @@ public:
     string sortVowels(string s) {
         int n = s.length();
         vector<char>vowels;
+        string v = "AEIOUaeiou";
         for(int i = 0 ; i < n ; i++ ){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+            if(v.find(s[i]) != string::npos){
                 vowels.push_back(s[i]);
             }
         }
         sort(vowels.begin(),vowels.end());
         int j = 0;
         for(int i = 0 ; i < n ; i++ ){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+            if(v.find(s[i]) != string::npos){
                 s[i] = vowels[j++];
             }
         }
