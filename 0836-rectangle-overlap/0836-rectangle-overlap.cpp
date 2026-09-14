92557374152bcc0ebab1ef34694a3cd7b73102c8
Yashi Saini <yashisaini718@gmail.com>
2026-09-15 00:33:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (47.04%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-// TOP-RIGHT of rec1 should be greater than BOTTOM-LEFT of rec2
+// TOP-RIGHT of rec1 should be greater than BOTTOM-LEFT of rec2 and BOTTOM-LEFT of rec1 should be less than TOP-RIGHT of rec2
+
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
         if ((rec1[2] > rec2[0] && rec1[0] < rec2[2]) && (rec1[3] > rec2[1] && rec1[1] < rec2[3]))return true;
         return false;
     }
+    
 };
