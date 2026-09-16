8a90d12cc6c2f9c0226b6a23852a4b377de3e69f
Yashi Saini <yashisaini718@gmail.com>
2026-09-16 19:46:58 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,10 +4,11 @@ public:
         int n = nums.size();
         int first = lower_bound(nums.begin(), nums.end(), target) - (nums.begin());
         if (first  == n) return {-1,-1};
+
         int last = upper_bound(nums.begin(),nums.end(), target) - nums.begin();
-        if ((first == last) && (nums[first] == target)) return {first,first};
+
+        if (first == last && nums[first] == target) return {first,first};
         else if (nums[first] == target && nums[last-1] == target) return {first,last-1};
         else return {-1,-1};
-        
     }
 };
