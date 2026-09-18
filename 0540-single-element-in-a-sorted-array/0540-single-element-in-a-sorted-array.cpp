e994d0f3bb1da830d7cb338834f726487b380d2e
Yashi Saini <yashisaini718@gmail.com>
2026-09-18 19:54:44 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     // check for parity of index where two same digits arrive like odd-even places or even-odd places check parity in start if after mid parity changes means single element already arrived else still not arrived
 
+
+// example dry run nums = [1,1,2,3,3,4,4,8,8]
     bool checkParityChange(int mid, vector<int>& nums) {
+
+        // mid points index 4 now nums[mid] == nums[mid-1] == 3 but mid is not odd so first if false secondly nums[mid] == nums[mid + 1] condition false and hence parity changed means single element is in first half
+
         if(nums[mid] == nums[mid - 1] && mid % 2 != 0) return false;
         else if (nums[mid] == nums[mid + 1] && mid % 2 == 0) return false;
         return true;
@@ -13,7 +18,7 @@ public:
         bool evenodd = false;
 
         if (n == 1) return nums[0];
-        
+
         if(nums[0] == nums[1]) evenodd = true;
         else return nums[0];
 
